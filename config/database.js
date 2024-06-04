6401eaaa46c0179d2d13fa165e3445d869dc6bad
Bahadur <bk209203@gmail.com>
2024-06-04 19:01:40 +0530

    done

--- config/database.js
+++ config/database.js
@@ -4,18 +4,18 @@ require('dotenv').config(); // Load environment variables
 
 // Database connection details
 const dbConfig = {
-  username: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_USERNAME :
-    process.env.NODE_ENV === 'test' ? process.env.TEST_DB_USERNAME :
-      process.env.DEV_DB_USERNAME,
-  password: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_PASSWORD :
-    process.env.NODE_ENV === 'test' ? process.env.TEST_DB_PASSWORD :
-      process.env.DEV_DB_PASSWORD,
-  database: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_NAME :
-    process.env.NODE_ENV === 'test' ? process.env.TEST_DB_NAME :
-      process.env.DEV_DB_NAME,
-  host: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_HOST :
-    process.env.NODE_ENV === 'test' ? process.env.TEST_DB_HOST :
-      process.env.DEV_DB_HOST,
+  username: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_USERNAME : 
+             process.env.NODE_ENV === 'test' ? process.env.TEST_DB_USERNAME : 
+             process.env.DEV_DB_USERNAME,
+  password: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_PASSWORD : 
+             process.env.NODE_ENV === 'test' ? process.env.TEST_DB_PASSWORD : 
+             process.env.DEV_DB_PASSWORD,
+  database: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_NAME : 
+             process.env.NODE_ENV === 'test' ? process.env.TEST_DB_NAME : 
+             process.env.DEV_DB_NAME,
+  host: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_HOST : 
+             process.env.NODE_ENV === 'test' ? process.env.TEST_DB_HOST : 
+             process.env.DEV_DB_HOST,
   dialect: 'mysql', // Assuming the same dialect for all environments
 };
 
@@ -24,10 +24,9 @@ const dbConfig = {
   try {
     // Create a connection without a specific database
     const connection = await mysql.createConnection({
-      username: process.env.PROD_DB_USERNAME,
-      password: process.env.PROD_DB_PASSWORD,
-      database: process.env.PROD_DB_NAME,
-      host: process.env.PROD_DB_HOST,
+      host: dbConfig.host,
+      user: dbConfig.username,
+      password: dbConfig.password,
     });
 
     // Create the database if it doesn't exist
@@ -66,7 +65,7 @@ const sequelize = new Sequelize(
   } catch (error) {
     console.error("Error synchronizing database:", error);
   }
-})();
+})(); 
 
 // Export the Sequelize instance for use in other parts of your application
 module.exports = sequelize;
