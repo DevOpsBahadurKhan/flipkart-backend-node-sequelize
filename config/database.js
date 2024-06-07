5c46e4ee9b8c62f4bf77a6d9ca50efe3173d6003
Bahadur <bk209203@gmail.com>
2024-06-07 15:47:27 +0530

    Done

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ require('dotenv').config(); // Load environment variables from .env file
 
 // Database configuration
 const dbConfig = {
+  database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: 'mysql',
 };
